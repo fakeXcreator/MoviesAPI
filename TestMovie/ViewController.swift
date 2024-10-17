e98f9982367ecaf6c5281a9dd29536d0e6aabc9d
fakeXcreator <fakexcreator@gmail.com>
2024-10-17 19:28:17 +0500

    Initial Commit

---
+++ TestMovie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestMovie
+//
+//  Created by Daniil Kim on 17.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
